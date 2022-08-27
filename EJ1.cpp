575e49c4b86259ba8b51064f9e697182685acfd9
Kz4w <sbmamanih@unjbg.edu.pe>
2022-08-26 19:54:48 -0500

    deleting

--- EJ1.cpp
+++ EJ1.cpp
@@ -1,63 +1,41 @@
 #include <iostream>
 #include <unistd.h>
 #include <thread>
-#include "ink.h"
+#include "monitor.h"
 
 using namespace std;
 // #-----------------------------------#
-class Incremento : public Monitor {
-  private:
-    int count;
-  public:
-    Incremento();
-    ~Incremento();
-    void inc();
-    void viewCount();
-};
-Incremento::Incremento() : Monitor() {
-  count = 0;
-}
-Incremento::~Incremento(){}
+Monitor Incremento;
+int counter = 0;
 
-void Incremento::inc() {
-  wait_m_mutex();
-  count += 2;
-  if (get_m_next_count() > 0) {
-    signal_m_next();
-  }
-  else signal_m_mutex();
+void inc() {
+    counter += 2;
 }
-void Incremento::viewCount(){
-  wait_m_mutex();
-  int value = count;
-  cout << value <<endl;
-  if (get_m_next_count() > 0) {
-    signal_m_next();
-  }
-  else signal_m_mutex();
-}
-// #-----------------------------------#
-void incrementar (Incremento* x) {
-  while (1) {
-    x->inc();
-    sleep(1);
-  }
+void viewCount() {
+    cout << "C : " << counter <<endl;
 }
 
-void imprimir (Incremento* x) {
-  while (1) {
-    x->viewCount();
-    sleep(1);
-  }
+void incrementar() {
+    while (true) {
+        Incremento.run([&](){
+            inc();
+        });
+        sleep(1);
+    }
+}
+void imprimir() {
+    while (true) {
+        Incremento.run([&](){
+            viewCount();
+        });
+        sleep(1);
+    }
 }
 int main () {
-  Incremento* x = new Incremento();
   thread Prcs[2];
-  Prcs[0] = thread(incrementar,x);
-  Prcs[1] = thread(imprimir,x);
-
+  Prcs[0] = thread(incrementar);
+  Prcs[1] = thread(imprimir);
   Prcs[0].join();
   Prcs[1].join();
-  delete x;
   return 0;
 }
